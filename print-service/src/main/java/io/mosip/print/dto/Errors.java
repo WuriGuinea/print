eff7e6ea24d139bb6b84dd8426baf63a874134ab
Chithara27 <chitharagowda27@gmail.com>
2023-06-14 10:54:17 +0530

    Print Changes added for guinea pilot.

---
+++ print-service/src/main/java/io/mosip/print/dto/Errors.java
@@ -0,0 +1,14 @@
+package io.mosip.print.dto;
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Errors {
+	String errorCode;
+	String message;
+
+}
