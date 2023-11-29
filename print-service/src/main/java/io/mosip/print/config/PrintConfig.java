106889e0bab31a912a312d7fb2b8861e2f210339
condeis <condeis@localhost>
2023-11-29 12:32:32 +0000

    bean activation

--- print-service/src/main/java/io/mosip/print/config/PrintConfig.java
+++ print-service/src/main/java/io/mosip/print/config/PrintConfig.java
@@ -1,12 +1,15 @@
 package io.mosip.print.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 
 @Configuration
+@ComponentScan(
+basePackages = { "io.mosip.commons.khazana.*"})
 public class PrintConfig {
 
     @Bean
